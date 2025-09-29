e2700cf570ced4b956d9c06b1b5f7bef84677574
saurav9315-coder <sauravmamdal655@gmail.com>
2025-09-29 21:15:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,152 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
+
+ScrollSmoother.create({
+  wrapper: "#smooth-wrapper",
+  content: "#smooth-content",
+  smooth: 4,            // Smoothness (higher is slower)
+  effects: true,          // Enable data-speed & data-lag
+});
+
+
+let isShortHeight = window.screen.height < 1050;  // this is only tp make animation acccording to screen height but not requrired.. 
+// you can remove or change this value according to your requrement
+// console.log(window.screen.height);
+
+ScrollTrigger.matchMedia({
+  "(min-width: 991px)": function () {
+
+    gsap.to('#headphone', {
+      scrollTrigger: {
+        trigger: '#section2',
+        start: 'top bottom',
+        end: 'center center',
+        scrub: true,
+      },
+      y: '85vh',
+      x: '18vw',
+      width: '32vw',
+      rotate: 90,
+      ease: 'power1.inOut',
+      immediateRender: false
+    });
+
+    gsap.to('#headphone', {
+      scrollTrigger: {
+        trigger: '#section3',
+        start: 'top bottom',
+        end: 'bottom bottom',
+        scrub: true,
+      },
+      y: '218vh',
+      x: '0',
+      width: '35vw',
+      rotate: 35,
+      ease: 'power1.inOut',
+      immediateRender: false
+    });
+
+    gsap.to('#headphone', {
+      scrollTrigger: {
+        trigger: '#section4',
+        start: 'top bottom',
+        end: 'center center',
+        scrub: true,
+      },
+      y: '308vh',
+      width: '42vw',
+      rotate: 0,
+      ease: 'power1.inOut',
+      immediateRender: false
+    });
+
+    gsap.to('#headphone', {
+      scrollTrigger: {
+        trigger: '#section5',
+        start: 'top bottom',
+        end: 'center bottom',
+        scrub: true,
+        // x: () => poductCard.getBoundingClientRect().left + window.screenX + 50,
+        // y: () => poductCard.getBoundingClientRect().top + window.screenY + 50,
+      },
+      y: isShortHeight ? '440vh' : '390vh',
+      width: '22vw',
+      ease: 'power1.inOut',
+      immediateRender: false
+    });
+
+    // content animation
+
+    gsap.from('#section2 .content-wrapper', {
+      scrollTrigger: {
+        trigger: '#section2',
+        start: '-50% bottom',
+        end: 'center center',
+        scrub: true,
+      },
+      y: '140%',
+      ease: 'power1.inOut',
+    });
+
+    gsap.from('#section3 .heading', {
+      scrollTrigger: {
+        trigger: '#section3',
+        start: 'top bottom',
+        end: 'center bottom',
+        scrub: true,
+      },
+      y: '140%',
+      ease: 'power1.inOut',
+    });
+
+    gsap.from('#section4 img', {
+      scrollTrigger: {
+        trigger: '#section4',
+        start: 'top bottom',
+        end: 'center center',
+        scrub: true,
+      },
+      width: 0,
+      opacity: 0,
+      ease: 'power1.inOut',
+    });
+
+    gsap.from('#section6 .content-wrapper', {
+      scrollTrigger: {
+        trigger: '#section6',
+        start: 'top bottom',
+        end: 'center center',
+        scrub: true,
+      },
+      y: '40%',
+      duration: 2,
+      ease: 'power1.inOut',
+    });
+
+    // hero section text animation
+    let split = SplitText.create('#section1 .heading', {
+      type: 'chars, words, lines',
+      mask: 'lines'
+    });
+
+    gsap.from(split.chars, {
+      yPercent: () => gsap.utils.random(-100, 100),
+      rotation: () => gsap.utils.random(-30, 30),
+      autoAlpha: 0,
+      ease: 'back.out(1.5)',
+      stagger: {
+        amount: 0.5,
+        from: 'random'
+      },
+      duration: 1.5
+    });
+
+    gsap.from('#headphone', {
+      opacity: 0,
+      scale: 0,
+      duration: 1,
+      delay: 1,
+      ease: 'power1.inOut'
+    })
+
+  }
+})
